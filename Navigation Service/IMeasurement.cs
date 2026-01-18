9e2d5c6b676b0f658b2bb1ab1ed9b72a00daba82
Hanan Gershon Cohen <hanang.cohen@gmail.com>
2026-01-19 00:22:09 +0200

    PositionArrivedEventArgs reaise event type IMeasurement .

--- Navigation Service/IMeasurement.cs
+++ Navigation Service/IMeasurement.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Navigation_Service
 {
-    internal interface INavigationDevice
+    public interface IMeasurement
     {
-        // every device must implement this event !
-        event EventHandler<PositionArrivedEventArgs> onPositionArrived;
+        double TimeStamp { get; }
     }
 }
---
+++ Navigation Service/PositionArrivedEventArgs.cs
@@ -0,0 +1,12 @@
+﻿namespace Navigation_Service
+{
+    internal class PositionArrivedEventArgs  : EventArgs
+    {
+        public  IMeasurement _position { get; private set; }
+
+        public PositionArrivedEventArgs(IMeasurement position)
+        {
+            _position = position;
+        }
+    }
+}
